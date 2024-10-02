7fe7f3136dfa52a36309365e431643336020bf7a
AlanaNeri <alananeri@outlook.com>
2024-10-02 11:27:55 -0300

    Currículo inicial

---
+++ script.js
@@ -0,0 +1,14 @@
+
+      let email = "alananeri@outlook.com";
+      let idade = "32 anos";
+      let endereco = "Travessa do Brejinho, nº 11 E, Ilha Amarela, Salvador-BA";
+      let contato = "(71) 98258-8652  | (71) 99130-8969"
+
+      function mostrarResultado(){
+          document.getElementById("info").innerHTML = 
+            
+          `Endereço: ${endereco} <br> Contato: ${contato} 
+          <br> e-mail: ${email}`
+          
+      }
+   
